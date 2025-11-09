b178d3925aef334523ef84abe363c8231c5c5bbd
Yevhen Kazanzhy <z.kazanzhi2017@gmail.com>
2025-11-09 23:16:08 +0200

    add Entities and Dtos

---
+++ backend/Librium.Domain/Dtos/BookCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librium.Domain.Dtos
+{
+    public class BookCategoryDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ backend/Librium.Domain/Entities/Books/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Librium.Domain.Entities.Books
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int CategoryId { get; set; }
+        public BookCategory BookCategory { get; set; }
+        public string Content { get; set; }
+        public int PublishedYear { get; set; }
+
+        [JsonIgnore]
+        public ICollection<UserBook> UserBooks { get; set; } = new List<UserBook>();
+    }
+}
