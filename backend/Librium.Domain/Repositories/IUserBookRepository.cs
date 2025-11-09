ddc4ab952811333ca1d9fa080d14980019321121
Yevhen Kazanzhy <z.kazanzhi2017@gmail.com>
2025-11-09 23:25:02 +0200

    add repository interfaces

---
+++ backend/Librium.Domain/Repositories/IUserBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Librium.Domain.Entities.Books;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librium.Domain.Repositories
+{
+    public interface IUserBookRepository
+    {
+        Task<List<UserBook>> GetUserBooksAsync(string userId);
+        Task<int> AddUserBookAsync(string userId, int bookId);
+        Task<int> RemoveUserBookAsync(string userId, int bookId);
+    }
+}
