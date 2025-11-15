4fa627a789e1fe1d98cd8e3534fb9ea598e81602
Yevhen Kazanzhy <z.kazanzhi2017@gmail.com>
2025-11-15 17:59:46 +0200

    refactor(persistence): move entity configurations to Fluent API

---
+++ backend/Librium.Persistence/Configurations/BookCategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Librium.Domain.Entities.Books;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Librium.Persistence.Configurations;
+
+public class BookCategoryConfiguration : IEntityTypeConfiguration<BookCategory>
+{
+    public void Configure(EntityTypeBuilder<BookCategory> builder)
+    {
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(20);
+    }
+}
---
+++ backend/Librium.Persistence/Configurations/UserBookConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Librium.Domain.Users.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Librium.Persistence.Configurations;
+
+public class UserBookConfiguration : IEntityTypeConfiguration<UserBook>
+{
+    public void Configure(EntityTypeBuilder<UserBook> builder)
+    {
+        builder.HasOne(ub => ub.AppUser)
+            .WithMany(u => u.UserBooks)
+            .HasForeignKey(ub => ub.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(ub => ub.Book)
+            .WithMany(b => b.UserBooks)
+            .HasForeignKey(ub => ub.BookId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
