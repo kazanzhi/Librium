fe59daced2ff5b4c74a2f9517a713c48e0f2146a
Yevhen Kazanzhy <z.kazanzhi2017@gmail.com>
2025-11-16 10:57:02 +0200

    move di registration to Application and Persistence layers

---
+++ backend/Librium.Persistence/DependencyInjection.cs
@@ -0,0 +1,62 @@
+﻿using Librium.Application.Interfaces;
+using Librium.Domain.Repositories;
+using Librium.Domain.Users.Models;
+using Librium.Persistence.Repositories;
+using Librium.Persistence.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace Librium.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<LibriumDbContext>(options =>
+            options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IBookCategoryRepository, BookCategoryRepository>();
+        services.AddScoped<IBookRepository, BookRepository>();
+        services.AddScoped<IUserBookRepository, UserBookRepository>();
+
+
+        services.AddIdentity<AppUser, IdentityRole>(options =>
+        {
+            options.User.RequireUniqueEmail = true;
+        })
+            .AddEntityFrameworkStores<LibriumDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.AddScoped<IIdentityService, IdentityService>();
+
+
+        services.AddAuthentication(options =>
+        {
+
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = config["JwtOptions:Issuer"],
+                ValidAudience = config["JwtOptions:Audience"],
+                RoleClaimType = ClaimTypes.Role,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["JwtOptions:Key"]))
+            };
+        });
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+
+        return services;
+    }
+}
