a60bc54e31b5c19ec5421a6ea0f429b1ca601e2f
Yevhen Kazanzhy <z.kazanzhi2017@gmail.com>
2025-11-19 16:21:29 +0200

    feature: add IAppUserRepository in domain layer and AppUserRepository in persistence layer

---
+++ backend/Librium.Persistence/Repositories/AppUserRepository.cs
@@ -0,0 +1,26 @@
+﻿using Librium.Domain.Users.Models;
+using Librium.Domain.Users.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Librium.Persistence.Repositories;
+
+public class AppUserRepository : IAppUserRepository
+{
+    private readonly LibriumDbContext _context;
+    public AppUserRepository(LibriumDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<AppUser?> GetUserWithBooks(string userId)
+    {
+        return await _context.Users
+            .Include(x => x.UserBooks)
+            .ThenInclude(x => x.Book)
+            .FirstOrDefaultAsync(x => x.Id == userId);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
