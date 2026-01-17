15856f6ea7c798c8bf443c4410a58d1cde6c5fc0
Yevhen Kazanzhy <z.kazanzhi2017@gmail.com>
2026-01-17 19:51:11 +0200

    feat(backend): add CommentController

---
+++ backend/Librium.Presentation/Controllers/CommentController.cs
@@ -0,0 +1,80 @@
+﻿using Librium.Application.Abstractions.Services;
+using Librium.Application.DTOs.Comments;
+using Librium.Identity.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Librium.Presentation.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        private Guid GetUserId()
+        {
+            return Guid.Parse(
+                User.FindFirstValue(ClaimTypes.NameIdentifier)!
+            );
+        }
+
+        [HttpPost("books/{bookId:guid}/comments")]
+        [Authorize(Roles = UserRoles.User)]
+        public async Task<IActionResult> Create([FromBody] CommentDto dto, Guid bookId)
+        {
+            var userId = GetUserId();
+            var result = await _commentService.Create(userId, bookId, dto);
+
+            return result.IsSuccess
+                ? CreatedAtAction(nameof(GetById), new { commentId = result.Value }, result.Value)
+                : BadRequest(result.ErrorMessage);
+        }
+
+        [HttpDelete("comments/{commentId:guid}")]
+        [Authorize(Roles = UserRoles.User)]
+        public async Task<IActionResult> Delete(Guid commentId)
+        {
+            var userId = GetUserId();
+            var result = await _commentService.Delete(commentId, userId);
+
+            return result.IsSuccess
+                ? NoContent()
+                : BadRequest(result.ErrorMessage);
+        }
+
+        [HttpGet("comments/{commentId:guid}")]
+        public async Task<IActionResult> GetById(Guid commentId)
+        {
+            var result = await _commentService.GetById(commentId);
+
+            return result.IsSuccess
+                ? Ok(result.Value)
+                : NotFound(result.ErrorMessage);
+        }
+
+        [HttpGet("books/{bookId:guid}/comments")]
+        public async Task<IActionResult> GetForBook(Guid bookId)
+        {
+            var comments = await _commentService.GetForBook(bookId);
+            return Ok(comments);
+        }
+
+        [HttpPut("comments/{commentId:guid}")]
+        [Authorize(Roles = UserRoles.User)]
+        public async Task<IActionResult> Update(Guid commentId, [FromBody] CommentDto dto)
+        {
+            var userId = GetUserId();
+            var result = await _commentService.Update(commentId, userId, dto);
+
+            return result.IsSuccess
+                ? NoContent()
+                : BadRequest(result.ErrorMessage);
+        }
+    }
+}
